a9eff76c1686360fbc9a9d9779d3cd2e5baf5e35
Yashavant Patil <yashavant.patil23@gmail.com>
2020-08-16 10:11:07 +0530

    Implemented hash router in home page

--- src/components/home_page/HomePage.js
+++ src/components/home_page/HomePage.js
@@ -1,8 +1,30 @@
 import React from "react";
+import {Switch, HashRouter} from "react-router-dom";
+import Title from "../common/title/Title";
 
 function HomePage() {
   return (
-    <h1>Home page</h1>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm-1">
+          <ul className="nav nav-pills flex-column">
+            <li className="nav-item">
+              <h6 className="nav-link">Pages</h6>
+            </li>
+          </ul>
+        </div>
+        <div className="col-sm-11">
+          <Switch>
+            <HashRouter exact path="/">
+              <Title
+                title="Home page"
+                subtitle="Please select example from list from side navigation."
+              />
+            </HashRouter>
+          </Switch>
+        </div>
+      </div>
+    </div>
   );
 }
 
