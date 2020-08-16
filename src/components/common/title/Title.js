85b02de3b9ba6fd0a33c7b13f1b8d6b448faeb6c
Yashavant Patil <yashavant.patil23@gmail.com>
2020-08-16 10:08:22 +0530

    Added title component

---
+++ src/components/common/title/Title.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Proptypes from "prop-types";
+
+function Title({title, subtitle}) {
+  return (
+    <div className="jumbotron text-center">
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
+    </div>
+  );
+}
+
+Title.propeTypes = {
+  title: Proptypes.string.isRequired,
+  subtitle: Proptypes.string,
+};
+
+Title.defaultProps = {
+  subtitle: '',
+};
+
+export default React.memo(Title);
