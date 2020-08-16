b16491817e8b63cd8dd7f2d04808e0f2c6df94f6
Yashavant Patil <yashavant.patil23@gmail.com>
2020-08-16 12:21:20 +0530

    Added to do page

---
+++ src/components/todo/TodoPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Title from "../common/title/Title";
+
+function TodoPage() {
+  return (
+    <>
+      <Title
+        title="Todo App"
+        subtitle="This example demonstrate the use of use reducer hook."
+      />
+    </>
+  );
+}
+
+export default TodoPage;
