031dbe8b67396dc8653e5e7f870d726796474b1c
Yashavant Patil <yashavant.patil23@gmail.com>
2020-08-16 10:58:11 +0530

    Added counter component

---
+++ src/components/counter/Counter.js
@@ -0,0 +1,50 @@
+import React, {useRef} from "react";
+import Proptypes from "prop-types";
+
+function Counter({title, count, incrementCount, decrementCount}) {
+  const interval = useRef(6);
+
+  const startAction = (action) => {
+    if (interval.current) clearInterval(interval.current);
+    interval.current = setInterval(action, 300);
+  }
+
+  const endAction = () => {
+    clearInterval(interval.current);
+  }
+
+  return (
+    <div className="container d-flex justify-content-center">
+      <div className="flex-column">
+        <h4 className="text-center">{title} : {count}</h4>
+        <div className="row">
+          <button
+            type="button"
+            className="btn btn-success"
+            onMouseDown={() => startAction(incrementCount)}
+            onMouseUp={() => endAction()}
+            onClick={incrementCount}>
+            Increment
+          </button>
+          <button
+            type="button"
+            className="btn btn-danger mx-2"
+            onMouseDown={() => startAction(decrementCount)}
+            onMouseUp={() => endAction()}
+            onClick={decrementCount}>
+            Decrement
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Counter.prototype = {
+  title: Proptypes.string.isRequired,
+  count: Proptypes.number.isRequired,
+  incrementCount: Proptypes.func.isRequired,
+  decrementCount: Proptypes.func.isRequired,
+};
+
+export default React.memo(Counter);
